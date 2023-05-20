f7c33f473379cf22b0a3f7cedceda937a7b7632d
tosiron <tosironj@gmail.com>
2023-05-20 21:55:13 +0100

    added search filters for company, name and featured

--- Controller/products.js
+++ Controller/products.js
@@ -1,15 +1,25 @@
 const Products=require('../Models/product')
 
 const getAllProductsStatic=async (req,res)=>{
-     const products = await Products.find({featured:true })
+    const search='oo'
+     const products = await Products.find({
+        name:{$regex:search,$options:'i'}
+     })
     res.status(200).json({products, nbHits:products.length})
 }
 const getAllProducts =async (req,res)=>{
-    const {featured}= req.query
+    const {featured,company,name}= req.query
     const queryObj={}
 if (featured){
-    queryObj.featured=featured ==='true'? true : false
+    queryObj.featured=featured ==='true' ? true : false
 }
+if (company){
+    queryObj.company=company   
+}
+if (name){
+    queryObj.name={$regex:name,$options:'i'}   
+}
+
 
     const products = await Products.find(queryObj )
     res.status(200).json({products, nbHits:products.length})
