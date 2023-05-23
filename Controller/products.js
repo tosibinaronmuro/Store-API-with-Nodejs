bfbda971caf9a2d831099aff422612e73bd300f8
tosiron <tosironj@gmail.com>
2023-05-23 14:13:53 +0100

    feature: added filters for sort,skip,numerical filters

--- Controller/products.js
+++ Controller/products.js
@@ -1,29 +1,53 @@
-const Products=require('../Models/product')
+const Products = require("../Models/product");
 
-const getAllProductsStatic=async (req,res)=>{
-    const search='oo'
-     const products = await Products.find({
-        name:{$regex:search,$options:'i'}
-     })
-    res.status(200).json({products, nbHits:products.length})
-}
-const getAllProducts =async (req,res)=>{
-    const {featured,company,name}= req.query
-    const queryObj={}
-if (featured){
-    queryObj.featured=featured ==='true' ? true : false
-}
-if (company){
-    queryObj.company=company   
-}
-if (name){
-    queryObj.name={$regex:name,$options:'i'}   
-}
+const getAllProductsStatic = async (req, res) => {
+  const products = await Products.find({}).select('name price')
+  res.status(200).json({ products, nbHits: products.length });
+};
+const getAllProducts = async (req, res) => {
+  const { featured, company, name, sort,keys,greaterThanFilters,lessThanFilters } = req.query;
+  //   search query
+  const queryObj = {};
+  if (featured) {
+    queryObj.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObj.company = company;
+  }
+  if (name) {
+    queryObj.name = { $regex: name, $options: "i" };
+  }
+
+  let result = Products.find(queryObj);
+  if (sort) {
+    const sortList = sort.split(",").join(' ');
+    // console.log(sort, sortList);
+    result = result.sort(sortList)
+  }
+  else{
+    result= result.sort('createdAt')
+  }
+
+  if(keys){
+    const keyList = keys.split(",").join(' ');
+    result=result.select(keyList)
+  }
+   const page=Number(req.query.page) || 1
+    const limit=Number(req.query.limit) || 10
+    const skip= (page-1)*limit;
+    
+ result=result.skip(skip).limit(limit)
 
 
-    const products = await Products.find(queryObj )
-    res.status(200).json({products, nbHits:products.length})
+ if(greaterThanFilters){
+  result=result.where({ price: { $gt:greaterThanFilters}});
+}
+ if(lessThanFilters){
+  result=result.where({ price: { $lt:lessThanFilters}});
 }
 
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
+};
 
-module.exports={getAllProducts,getAllProductsStatic}
+module.exports = { getAllProducts, getAllProductsStatic };
